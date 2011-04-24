7062542288622f816d19526a34e1db54cf5b9d47
Joe Wasson <jwasson+git@gmail.com>
2011-04-24 15:02:21 -0700

    Allow releasing without a tag. Only allow releasing from master.

--- release.cmd
+++ release.cmd
@@ -1,7 +1,8 @@
 @echo off
 IF "%1"=="" (
-  ECHO No version number supplied.
+  tools\nant\nant.exe publish
   GOTO :EOF
 )
 
-tools\nant\nant.exe tag dist publish -D:release.version=%1
+tools\nant\nant.exe tag publish -D:release.version=%1
+
